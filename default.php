fcc76f36d8bffaec51d3f2b25ad82186f5e13ec0
andrejusnec <73877307+andrejusnec@users.noreply.github.com>
2021-03-13 15:09:48 +0200

    upd

--- default.php
+++ default.php
@@ -3,6 +3,7 @@ session_start();
 define('URL','http://localhost/php_proj/OOPbank/');
 define('INSTALL_DIR','/php_proj/OOPbank/');
 define('DIR',__DIR__);
+define('CSS', 'C:\xampp\htdocs\php_proj\OOPbank\public\css');
 //require DIR.'/function.php';
 require DIR.'/app/UserCreate.php';
 require DIR.'/app/Json.php';
